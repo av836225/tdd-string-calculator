cdad7992fd21a4097c49e611b76b856fe6c4c419
Abhishek Verma <abhishek.verma@republicworld.com>
2025-03-21 18:04:07 +0530

    passing test case for string with comma seperated

--- string_calculator.js
+++ string_calculator.js
@@ -1,5 +1,14 @@
 function add(numbers) {
+    
+    // if the input is empty return 0
+    if (!numbers) return 0;
 
+    // if the input is a single or multiple number return the number or sum of the numbers
+    let delimiter = /,/;
+    const numArray = numbers.split(delimiter).map(Number);
+
+    return numArray.reduce((sum, num) => sum + num, 0);
+ 
 
 }
 
