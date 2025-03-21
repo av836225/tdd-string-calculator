9e082062ff87b636fd32f2df715b19bdea48d791
Abhishek Verma <abhishek.verma@republicworld.com>
2025-03-21 18:06:32 +0530

    passing test case for string start with delimiter

--- string_calculator.js
+++ string_calculator.js
@@ -5,6 +5,13 @@ function add(numbers) {
 
     // if the input is a single or multiple number return the number or sum of the numbers
     let delimiter = /,|\n/;
+    
+    // if the input starts with // then the delimiter is the character after the //
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts.slice(1).join("\n");
+    }
     const numArray = numbers.split(delimiter).map(Number);
 
     return numArray.reduce((sum, num) => sum + num, 0);
